218d4e2de190af99f6f84a18b0b79678e7d092ff
Kawanbatra <74811069+Kawanbatra@users.noreply.github.com>
2020-11-22 00:17:05 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Avatar from "react-avatar";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <Avatar
+          name="Kawan Batra"
+          round
+          src="https://scontent.fdel1-4.fna.fbcdn.net/v/t1.0-9/117597029_968710186887017_8876443496544762021_o.jpg?_nc_cat=108&ccb=2&_nc_sid=8bfeb9&_nc_ohc=xyP9gHWwasIAX8LZZGy&_nc_ht=scontent.fdel1-4.fna&oh=7e5c85eda4100042386b0c0fbafcbb37&oe=5FDF035C"
+        />
+        <h1>Kawan Batra</h1>
+      </div>
+      <hr />
+      <h2>Education</h2>
+      <ul>
+        <li>
+          Persuing Bachelor of computer application from Tilak Raj Chaddha
+          Institute of management And Technology affiliated from Kurukshetra
+          university.
+        </li>
+        <li>
+          Persued secondary education and matriculation from Rainbow School
+          affliated from CBSE.
+        </li>
+      </ul>
+      <hr />
+      <h2>Objective</h2>
+      <ul>
+        <li>
+          Looking forward to an opportunity for working in a dynamic,
+          challenging environment, where I can utilize my skills and training
+          for developing my career and making a significant success and growth
+          of the organization.
+        </li>
+      </ul>
+      <hr />
+      <h2>Software/computer Proficiency</h2>
+      <ul>
+        <li>Computer languages:C,C++</li>
+        <li>Web Technologies:HTML,CSS,javascript</li>
+        <li>Database:Mysql</li>
+      </ul>
+      <hr />
+      <h2>Awards/Achievements</h2>
+      <ul>
+        <li>
+          Participated in Declamation contests and extra Co –Cricular activities
+        </li>
+        <li>Participated and won in games</li>
+      </ul>
+      <h2>Interests/Hobbies</h2>
+      <ul>
+        <li>Reading, cooking, listening to music.</li>
+      </ul>
+      <hr />
+      <h2>Personal details</h2>
+      <ul>
+        <li>D.O.B:-03-09-1999</li>
+        <li>Nationality:-Indian</li>
+        <li>Religion:-Sikh</li>
+        <li>Languages Known:-Hindi,English</li>
+      </ul>
+      <hr />
+      <h2>Address</h2>
+      #2A/55 Gurudwara Road Opposite Telephone Exchange Saharanpur -247001 Uttar
+      Pradesh
+      <hr />
+      <h2>Declaration</h2>
+      I hereby declare that above information is correct to the best of my
+      knowledge and belief.
+      <hr />
+      <h2>Contact</h2>
+      <ul>
+        <li>Tel : (+91) 9817156230</li>
+        <li>e-mail : kawanbatra@gmail.com</li>
+      </ul>
+    </div>
+  );
+}
